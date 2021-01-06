5505545533c006f10943fc10c66c584dc0f6ac49
Jigar Soni <sonijigar94@gmail.com>
2021-01-06 21:34:20 +0530

    Update IncrementAndDecrementOperator.java

--- src/main/java/javaBasics/IncrementAndDecrementOperator.java
+++ src/main/java/javaBasics/IncrementAndDecrementOperator.java
@@ -4,32 +4,33 @@ public class IncrementAndDecrementOperator {
 
 	public static void main(String[] args) {
 		
-		//++
-		//--
-		
-		int i=1;
-		int j= i++; //post increment
-		
-		System.out.println(i);
-		System.out.println(j);
-		
-		int a=1;
-		int b =++a; //pre increment
-		
-		System.out.println(a);
-		System.out.println(b);
-		
-		int m =2;
-		int n = m--; //post decrement
-		
-		System.out.println(m);
-		System.out.println(n);
-		
-		int k = 2;
-		int l = --k; //pre decrement
-		
-		System.out.println(k);
-		System.out.println(l);
+
+        //++
+        //--
+
+        int i=1;
+        int j= i++; //post increment
+
+        System.out.println(i);//2
+        System.out.println(j);//1
+
+        int a=1;
+        int b =++a; //pre increment
+
+        System.out.println(a);//2
+        System.out.println(b);//2
+
+        int m =2;
+        int n = m--; //post decrement
+
+        System.out.println(m);//1
+        System.out.println(n);//2
+
+        int k = 2;
+        int l = --k; //pre decrement
+
+        System.out.println(k);//1
+        System.out.println(l);//1
 	}
 
 }
